fb665b640217c19e4b2bc0212fe1d1cd704cfe44
DeMo900 <dodoadam893@gmail.com>
2026-03-09 14:11:24 +0200

    clearing wins

---
+++ src/jobs/clearWins.ts
@@ -0,0 +1,14 @@
+import nodeCron from "node-cron";
+import User from "../models/user"
+
+
+
+
+nodeCron.schedule("0 0 * * 0", async() => {
+    try{
+await User.updateMany({},{$set:{wins:0}})
+console.log("all wins cleared")
+    }catch(err){
+        return console.log(`error while clearing wins`)
+    }
+})
