570fad9760f044a7ca6a1ddbadafd29c7c4598bf
DeMo900 <dodoadam893@gmail.com>
2026-03-09 13:59:42 +0200

    leadboard

--- src/controllers/predictionsController.ts
+++ src/controllers/predictionsController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import User from "../models/user"
 import Bet from '../models/bet';
 
 
-export default async function betController (req: Request, res: Response) {
+export async function betController (req: Request, res: Response) {
     try{
 //getting the user from the cookies
 const user = req.cookies["jwt"];
-if(!user) return res.status(401).json({message: "Unauthorized"});
 const {id,email} = jwt.verify(user, process.env.JWT_SECRET as string) as { id: string, email: string };
 const newBet = new Bet({
     gameId: req.body.gameId,
@@ -24,6 +24,17 @@ return res.status(201).json({message: "Bet placed successfully"});
     }
  }
 
+ export async function leadBoardController (req:Request,res:Response){
+try{
+const rankedUsers = await User.find().sort({wins:-1})
+.select("wins")
+.select("username")
+.select("coins")
+return res.json({msg:rankedUsers})
+}catch(err){
+return res.status(500).send("internal server error"+err)
+}
+ }
 
 //needed data to make a bet 
 //game id  	1380590
--- src/routes/predictionRoutes.ts
+++ src/routes/predictionRoutes.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
-import betController from '../controllers/predictionsController';
+import {betController,leadBoardController} from '../controllers/predictionsController';
 const predictionRouter = express.Router();
-
+//POST BET
 predictionRouter.post('/bet', betController);
-
+//GET LEADBOARD
+predictionRouter.get("/leadboard",leadBoardController)
 export default predictionRouter;
