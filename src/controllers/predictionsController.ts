e6fc2b609c60529e9838837f6274c818a7f6aacc
DeMo900 <dodoadam893@gmail.com>
2026-03-10 14:34:23 +0200

    paged system

--- src/controllers/predictionsController.ts
+++ src/controllers/predictionsController.ts
@@ -26,10 +26,16 @@ return res.status(201).json({message: "Bet placed successfully"});
 
  export async function leadBoardController (req:Request,res:Response){
 try{
+    const page = req.query.page
+if (!page || typeof page !== 'string') throw new Error("no page was provided") 
+ const parsedPage = parseInt(page)
+const max = 10
 const rankedUsers = await User.find().sort({wins:-1})
 .select("wins")
 .select("username")
 .select("coins")
+.limit(10)
+.skip(parsedPage-1*max)
 return res.json({msg:rankedUsers})
 }catch(err){
 return res.status(500).send("internal server error"+err)
--- src/jobs/clearWins.ts
+++ src/jobs/clearWins.ts
@@ -1,9 +1,6 @@
 import nodeCron from "node-cron";
 import User from "../models/user"
 
-
-
-
 nodeCron.schedule("0 0 * * 0", async() => {
     try{
 await User.updateMany({},{$set:{wins:0}})
